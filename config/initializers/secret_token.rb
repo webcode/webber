bafdf38240768727415e069c3bb96e058ddb5363
christian <christian@christian-laptop.(none)>
2012-11-16 12:47:38 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webber::Application.config.secret_token = '0453881482161c96f9c284f9f22fc48191b97ba98eb5533d1159e6976fb4414a7095eb5bde39ce2a7080c80cd13bf7f3af8679b30acc3e14c1fca13ec406df8d'
